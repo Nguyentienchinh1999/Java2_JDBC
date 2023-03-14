96c32719dc5bb53a1c0b6c7968d9102b64a796a7
chinh <bjergsen999@Gmail.com>
2023-03-14 16:43:03 +0700

    continue

--- src/main/java/Main/Main_employees.java
+++ src/main/java/Main/Main_employees.java
@@ -75,14 +75,64 @@ public class Main_employees {
         System.out.println(employeesList);
     }
 
+    private static void option4(Scanner in){
+        Employees em = new Employees();
+        System.out.println("nhập vào id nhân viên cần update: ");
+        int id = in.nextInt();
+        boolean flag = false;
+        for(int i = 0; i < emList.size(); i++){
+            if(emList.get(i).getId() != id){
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            System.out.println("Không tồn tại nhân viên này");
+            return;
+        }else{
+            System.out.println("nhập tên cần thay đổi: ");
+            em.setFull_name(in.nextLine());
+            System.out.println("nhập city cần thay đổi: ");
+            em.setCity(in.nextLine());
+            System.out.println("nhập email cần thay đổi: ");
+            String email = in.nextLine();
+            boolean flag1 = false;
+            for(int i  = 0; i < emList.size(); i++){
+                if(emList.get(i).getEmail().equalsIgnoreCase(email)){
+                    flag1 = true;
+                    break;
+                }
+            }
+            if(flag1){
+                System.out.println("đã tồn tại email: ");
+                return;
+            }else {
+                em.setEmail(email);
+            }
+            System.out.println("nhập vào phone: ");
+            em.setPhone(in.nextLine());
+            System.out.println("nhập vào gender: ");
+            int gender = in.nextInt();
+            if(gender != 0 && gender != 1){
+                System.out.println("vui lòng nhập 0 là nữ hoặc 1 là nam");
+                return;
+            }else {
+                em.setGender(gender);
+            }
+            System.out.println("nhập vào salary:");
+            em.setSalrary(in.nextInt());
+            employeesDAO.update(em, id);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int option = -1;
         do{
             mainMenu();
-            System.out.println("nhập lựa chọn");
+            System.out.print("nhập lựa chọn ");
             try{
-                option = Integer.parseInt(in.nextLine());
+                option = Integer.parseInt(in.next());
             }catch (Exception e){
                 System.out.println("nhap sai dinh dang");
                 continue;
@@ -102,7 +152,7 @@ public class Main_employees {
                     option3(in);
                     break;
                 case 4:
-
+                    option4(in);
                     break;
             }
         }while (option != 8);
